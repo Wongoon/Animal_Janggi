24c626f888b8ba47b55b482411d5b4de6ef4b3f9
Wongoon <rmsgh64@gmail.com>
2024-11-15 21:14:37 +0900

    Version Update

--- Animal_Janggi/Assets/Scripts/PanelManager.cs
+++ Animal_Janggi/Assets/Scripts/PanelManager.cs
@@ -1,20 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PanelManager : MonoBehaviour
 {
-    public GameObject panel;
+    public GameObject mainPanel;
+    public GameObject settingPanel;
+    public GameObject panelAll;
+    public bool panelActive;
 
     void Awake() {
-        panel.SetActive(false);
+        mainPanel.SetActive(panelActive);
+        
+        Time.timeScale = 0;
     }
 
-    public void PanelUP() {
-        panel.SetActive(true);
+    public void PanelUp(String panel) {
+        if (panel.Equals("main")) {
+            mainPanel.SetActive(true);
+        }
+        else if (panel.Equals("setting")) {
+            settingPanel.SetActive(true);
+        }
+        else if (panel.Equals("all")) {
+            panelAll.SetActive(true);
+        }
+        else {
+            Debug.Log("Up Error");
+        }
     }
 
-    public void PanelDown() {
-        panel.SetActive(false);
+    public void PanelDown(String panel) {
+        if (panel.Equals("main")) {
+            mainPanel.SetActive(false);
+        }
+        else if (panel.Equals("setting")) {
+            settingPanel.SetActive(false);
+        }
+        else if (panel.Equals("all")) {
+            panelAll.SetActive(true);
+        }
+        else {
+            Debug.Log("Down Error");
+        }
+    }
+    
+    public void ExitGame() {
+        Application.Quit();
     }
 }
